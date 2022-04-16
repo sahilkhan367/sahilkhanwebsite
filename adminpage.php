aa7caf47db9991c5a9b17431613d9bf94ab29957
sahil khan <sahilkhan995564@gmail.com>
2022-04-16 17:30:15 +0530

    first commit

---
+++ adminpage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>admin page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    <div class="container">
+        <div class="admin-product-form-container">
+            <form action="<?php $_SERVER['PHP_SELF'] ?>"methods="post" enctype="multipart/form-data">
+                <h3>add a new product</h3>
+                <input type="text" placeholder="enter product name" name="product_name" class="box">
+                <input type="number" placeholder="enter product price" name="product_price" class="box">
+                <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
+                <input type="submit" class="btn" name="add_product" value="add_product">
+            </form>
+        </div>
+    </div>
+
+
+        
+    
+        <script src="" async defer></script>
+    </body>
+</html>
